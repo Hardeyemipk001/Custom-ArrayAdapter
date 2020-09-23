9474a9f7021a24daf62643771bac73b40dbb4c21
Sherifdeen <Kaseemsherif@gmail.com>
2020-09-23 17:23:25 +0100

    Updated

--- app/src/main/java/com/example/android/miwok/Word.java
+++ app/src/main/java/com/example/android/miwok/Word.java
@@ -1,20 +1,34 @@
 package com.example.android.miwok;
 
 public class Word {
+    private int mImageResourceId;
     private  String mDefaultTranslation;
     private String mMiwokTranslation;
 
-    public Word (String vdefaultTranslation, String vMiwokTranslation){
+    public Word (String vdefaultTranslation, String vMiwokTranslation, int imageResourceId){
         mDefaultTranslation=vdefaultTranslation;
          mMiwokTranslation=vMiwokTranslation;
+         mImageResourceId = imageResourceId;
     }
     public String getmDefaultTranslation(){
         return mDefaultTranslation;
     }
+    public int getImageResourceId() {
+        return mImageResourceId;
+    }
     public  String getmMiwokTranslation(){
         return mMiwokTranslation;
     }
+    public Word (String vdefaultTranslation, String vMiwokTranslation){
+        mDefaultTranslation=vdefaultTranslation;
+        mMiwokTranslation=vMiwokTranslation;
+
+    }
+
  }
+
+
+
 //public class AndroidFlavor {
 //    private String mVersionName;
 //    private String mVersionNumber;
